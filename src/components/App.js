67c5932ff538cc4751269a397dc39915c6dd1aa2
wjdekdnlt <wjdekdnlt1215@gmail.com>
2020-09-16 15:12:52 +0900

    2-1

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import Router from "components/Router";
+import { authService } from "Icebase";
+
+function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
+  return (
+    <>
+      <Router isLoggedIn={isLoggedIn} />
+      <footer>&copy; {new Date().getFullYear()} Nwitter</footer>
+    </>
+  );
+}
+
+export default App;
